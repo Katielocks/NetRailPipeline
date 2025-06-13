fa6002146ff918cb3b5dbadfa070c55d471e724c
Katielocks <ktwhitelock@outlook.com>
2025-06-13 10:38:26 +0100

    Update __init__.py

--- src/rail_data/rail_io/__init__.py
+++ src/rail_data/rail_io/__init__.py
@@ -4,12 +4,24 @@ from .elr_weather import get_loc_elr,get_weather,get_loc_weather
 from .extract_delay import extract_delay_dataset
 from .extract_timetable import extract_timetable
 from .loc_client import get_location_codes,extract_location_codes
-from .nationalrail_client import NationalRailSession
-from .session import Session
+from .nationalrail_client import NationalRailSession as NatRaSes
 from .track_client import get_track
 from .uk_holidays_client import fetch_public_holidays
 from .utils import read_cache,write_cache
 
 __all__ = ["settings",
-           "get_corpus"
+           "get_corpus",
+           "get_loc_elr",
+           "get_weather",
+           "get_loc_weather",
+            "extract_delay_dataset",
+            "extract_timetable",
+            "get_location_codes",
+            "extract_location_codes",
+            "NatRaSes",
+            "get_track",
+            "fetch_public_holidays",
+            "read_cache",
+            "write_cache"
+
            ]
